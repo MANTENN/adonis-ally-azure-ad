74c4f665fb27a52400f6829061143a21388635ae
MANTENN <nzrbeats@gmail.com>
2023-05-15 23:17:51 -0700

    Update index.ts

--- providers/index.ts
+++ providers/index.ts
@@ -1,14 +1,28 @@
 import type { ApplicationContract } from '@ioc:Adonis/Core/Application'
 
-export default class AADProvider {
+export default class AppProvider {
   constructor(protected app: ApplicationContract) {}
 
+  public register() {
+    // Register your own bindings
+  }
+
   public async boot() {
+    // IoC container is ready
+    // Azure Active Directory Authentication
     const Ally = this.app.container.resolveBinding('Adonis/Addons/Ally')
-    const { AAD } = await import('../src/AzureAD')
+    const { AzureActiveDirectory } = await import('../src/AzureActiveDirectoryProvider')
 
     Ally.extend('AzureAD', (_, __, config, ctx) => {
-      return new AAD(ctx, config)
+      return new AzureActiveDirectory(ctx, config)
     })
+  }
+
+  public async ready() {
+    // App is ready
+  }
+
+  public async shutdown() {
+    // Cleanup, since app is going down
   }
 }
